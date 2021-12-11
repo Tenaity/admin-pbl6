9a7068161336f309dddcc7969c6a4a46d39fca48
Van Muoi <tenaiti.150320@gmail.com>
2021-12-11 16:28:56 +0700

    remove

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,26 +1,6 @@
-/*!
-
-=========================================================
-* Purity UI Dashboard PRO - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/purity-ui-dashboard-pro
-* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
-
-* Design by Creative Tim & Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 import { Flex, Link, List, ListItem, Text } from "@chakra-ui/react";
 import React from "react";
 
 export default function Footer() {
-  return (
-    <Flex>
-    </Flex>
-  );
+  return <Flex></Flex>;
 }
--- src/variables/general.js
+++ src/variables/general.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Purity UI Dashboard PRO - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/purity-ui-dashboard-pro
-* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
-
-* Design by Creative Tim & Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 // Assets
 import avatar1 from "assets/img/avatars/avatar1.png";
 import avatar10 from "assets/img/avatars/avatar10.png";
